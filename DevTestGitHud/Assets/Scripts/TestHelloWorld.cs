5feb4edf6b9b6ae2a281689249a6c6e30027b4ed
yn6366 <yn6366@gmail.com>
2019-05-02 16:25:56 +0900

    두번째 커밋

--- DevTestGitHud/Assets/Scripts/TestHelloWorld.cs
+++ DevTestGitHud/Assets/Scripts/TestHelloWorld.cs
@@ -7,7 +7,8 @@ public class TestHelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World!");
+        //Debug.Log("Hello World!");
+        Debug.Log("안녕하세요");
     }
 
     // Update is called once per frame
